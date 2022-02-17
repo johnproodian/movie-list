297e2c87cad9258b557b8f1ee1a7cfe9e826ad2f
svnday <40030806+svnday@users.noreply.github.com>
2022-02-17 10:46:46 -0600

    updates server.js to include most of the necessary dependencies to the controllers, as well as functionality to store sessions as cookies

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS movie_list_db;
+
+CREATE DATABASE movie_list_db;
---
+++ server.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+const hbs = exphbs = exphbs.create({});
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Secret cookie',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.status(path.join(__dirname, 'public')));
+
+
+
+
+
+// activates individual routes/controllers
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
