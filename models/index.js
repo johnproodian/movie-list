8dc6fd86a197f616f94f94a73f9d2cba147d0489
svnday <40030806+svnday@users.noreply.github.com>
2022-02-17 20:08:52 -0600

    User model created, indexjs file in models folder updated with some associations

--- models/index.js
+++ models/index.js
@@ -1,9 +1,17 @@
 const User = require('./User');
 
 
+// associations
+
+User.hasMany(hasWatched, {
+    foreignKey: 'hasWatched_movieID'
+});
+
+User.hasMany(willWatch, {
+    foreignKey: 'willWatch_movieID'
+});
 
 
 
 
-
-module.exports = { User }
+module.exports = { User, hasWatched, willWatch }
