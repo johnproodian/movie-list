fb7def9922c602dd0f5f3958ffa24481d59d5dc6
Jotas <jproodian721@gmail.com>
2022-02-17 20:33:48 -0600

    trying to get the server to start

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { User, haveWatched, willWatch } = require('../../models');
 // need to create this file...
 const sequelize = require('../config/connection');
 
