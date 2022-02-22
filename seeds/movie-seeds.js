ca05a81618a78a99a1aba9f9c497a72faa3b190c
Xavyr Rodriguez <xavyrrodriguez@Xavyrs-Air.attlocal.net>
2022-02-21 20:11:53 -0600

    added movies to seeds folder

--- seeds/movie-seeds.js
+++ seeds/movie-seeds.js
@@ -2,8 +2,41 @@ const { Movies } = require('../models')
 
 const moviesData = [
     {
-
-    }
+        movieName: 'Forest Gump'
+    },
+    {
+        movieName: 'Mean Girls'
+    },
+    {
+        movieName: 'A New Hope'
+    },
+    {
+        movieName: 'Spaceballs'
+    },
+    {
+        movieName: 'The Fifth Element'
+    },
+    {
+        movieName: 'Death Becomes Her'
+    },
+    {
+        movieName: 'The Breakfast Club'
+    },
+    {
+        movieName: 'Clueless'
+    },
+    {
+        movieName: 'The Wedding Singer'
+    },
+    {
+        movieName: '10 Things I Hate About You'
+    },
+    {
+        movieName: 'Scream'
+    },
+    {
+        movieName: 'Twister'
+    },
 ];
 
 const seedMovies = () => Movies.bulkCreate(moviesData);
