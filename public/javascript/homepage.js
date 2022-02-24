7b4527d243ee1c3a0414baa1db4ab8d25e293486
Jotas <jproodian721@gmail.com>
2022-02-24 10:34:46 -0600

    commented out homepage file

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -8,27 +8,27 @@
 //         getMovies(searchItem);
 //         searchItem.value = "";
 //     } else {
-//         alert("Please search for a movie");
-//     }
-// } 
+// //         alert("Please search for a movie");
+// //     }
+// // } 
 
-const getMovies = () => {
-    let response = fetch('/api/movies', {method: 'get'})
-    .then(console.log(response));
+// const getMovies = () => {
+//     let response = fetch('/api/movies', {method: 'get'})
+//     .then(console.log(response));
     
 
-    // if (response.ok) {
-    //     console.log('success');
-    //     response.json().then( movieData => {
-    //         displayMovies(movieData, movie);
-    //     });
-    // } else {
-    //     alert(response.statusText);
-    // }
-}
-
-// const displayMovies = (movies, searchTerm) {
-
+//     // if (response.ok) {
+//     //     console.log('success');
+//     //     response.json().then( movieData => {
+//     //         displayMovies(movieData, movie);
+//     //     });
+//     // } else {
+//     //     alert(response.statusText);
+//     // }
 // }
 
-getMovies();
+// // const displayMovies = (movies, searchTerm) {
+
+// // }
+
+// getMovies();
