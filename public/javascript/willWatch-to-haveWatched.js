4b9aa6861f24a1ec90c21ccba6014c47563fd61b
Jotas <jproodian721@gmail.com>
2022-02-26 23:48:45 -0600

    changed movies in each column to be buttons, added a better function to move movies from will watch to have watched columns

--- public/javascript/willWatch-to-haveWatched.js
+++ public/javascript/willWatch-to-haveWatched.js
@@ -2,82 +2,109 @@
 // See Search-to-willWatch.js --> should be very similar
 // Look at the PUT /haveWatched route in movie-routes
 
-function wwToHaveWatched(event) {
+// function wwToHaveWatched(event) {
     
-    console.log('worked!');
-    document.location.reload();
+//     console.log('worked!');
+//     document.location.reload();
 
-}
+// }
 
+const wwToHaveWatched = function() {
+    const movieName = event.target.textContent;
 
-
-
-// document.querySelector()
-
-
-
-// for (let i = 0; i < )
-
-
-// event handler for clicking on button on the movie
-if (document.querySelector("#ww-1")) {
-    document.querySelector("#ww-1").addEventListener("click", () => {
-        const movieName = "Forrest Gump";
-
-        fetch('/api/movies/haveWatched', {
-            method: 'PUT',
-            body: JSON.stringify({
-                movieName
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        })
-        .then(response => {
-            if (response.ok) {
-                document.location.replace('/');
-            } else {
-                alert(response.statusText);
-            }
-        })
-        .catch(err => {
-            alert('Unable to move movie to "Have Watched"');
-        })
+    fetch('/api/movies/haveWatched', {
+        method: 'PUT',
+        body: JSON.stringify({
+            movieName
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => {
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    })
+    .catch(err => {
+        alert('Unable to move movie to "Have Watched"');
     })
 }
 
-if (document.querySelector("#ww-2")) {
-    document.querySelector("#ww-2").addEventListener("click", () => {
-        const movieName = "Mean Girls";
+const wwButtons = document.getElementsByClassName('ww-btn');
 
-        fetch('/api/movies/haveWatched', {
-            method: 'PUT',
-            body: JSON.stringify({
-                movieName
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        })
-        .then(response => {
-            if (response.ok) {
-                document.location.replace('/');
-            } else {
-                alert(response.statusText);
-            }
-        })
-        .catch(err => {
-            alert('Unable to move movie to "Have Watched"');
-        })
-    })
+for (let i = 0; i < wwButtons.length; i++) {
+    wwButtons[i].addEventListener('click', wwToHaveWatched);
 }
 
-if (document.querySelector("#ww-3")) {
-    document.querySelector("#ww-3").addEventListener("click", wwToHaveWatched)
-}
 
-if (document.querySelector("#ww-4")) {
-    document.querySelector("#ww-4").addEventListener("click", wwToHaveWatched)
-}
 
-if (document.querySelector("#ww-5")) {
-    document.querySelector("#ww-5").addEventListener("click", wwToHaveWatched)
-}
+// // document.querySelector()
+
+
+
+// // for (let i = 0; i < )
+
+
+// // event handler for clicking on button on the movie
+// if (document.querySelector("#ww-1")) {
+//     document.querySelector("#ww-1").addEventListener("click", () => {
+//         const movieName = "Forrest Gump";
+
+//         fetch('/api/movies/haveWatched', {
+//             method: 'PUT',
+//             body: JSON.stringify({
+//                 movieName
+//             }),
+//             headers: { 'Content-Type': 'application/json' }
+//         })
+//         .then(response => {
+//             if (response.ok) {
+//                 document.location.replace('/');
+//             } else {
+//                 alert(response.statusText);
+//             }
+//         })
+//         .catch(err => {
+//             alert('Unable to move movie to "Have Watched"');
+//         })
+//     })
+// }
+
+// if (document.querySelector("#ww-2")) {
+//     document.querySelector("#ww-2").addEventListener("click", () => {
+//         const movieName = "Mean Girls";
+
+//         fetch('/api/movies/haveWatched', {
+//             method: 'PUT',
+//             body: JSON.stringify({
+//                 movieName
+//             }),
+//             headers: { 'Content-Type': 'application/json' }
+//         })
+//         .then(response => {
+//             if (response.ok) {
+//                 document.location.replace('/');
+//             } else {
+//                 alert(response.statusText);
+//             }
+//         })
+//         .catch(err => {
+//             alert('Unable to move movie to "Have Watched"');
+//         })
+//     })
+// }
+
+// if (document.querySelector("#ww-3")) {
+//     document.querySelector("#ww-3").addEventListener("click", wwToHaveWatched)
+// }
+
+// if (document.querySelector("#ww-4")) {
+//     document.querySelector("#ww-4").addEventListener("click", wwToHaveWatched)
+// }
+
+// if (document.querySelector("#ww-5")) {
+//     document.querySelector("#ww-5").addEventListener("click", wwToHaveWatched)
+// }
 
 
