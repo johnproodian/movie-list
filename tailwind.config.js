927d6ce511362d3a5c1a03660528415ae705d64a
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-02-27 14:30:04 -0600

    adding container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./views/layouts/main.handlebars"],
   theme: {
     extend: {},
+    container: {
+      center: true,
+    }
   },
   plugins: [
     require('@tailwindcss/forms')
